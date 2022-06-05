d27f9837d78d00c215454376fc8be24779f4fc6b
Matt Weinstein <mattw412@gmail.com>
2022-06-05 00:00:22 -0400

    Create Sum-of-Arrayys.js

---
+++ Arrays/Sum-of-Arrayys.js
@@ -0,0 +1,8 @@
+//Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+function sum (numbers) {  
+    if(numbers.length>0){
+    return numbers.reduce((acc,cur)=>acc+cur)
+      }
+  else{return 0}
+};
