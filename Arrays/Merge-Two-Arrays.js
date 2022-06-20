e88828d4e60cf3e919b9fc612d97eac56b2a2818
Matt Weinstein <mattw412@gmail.com>
2022-06-19 23:53:07 -0400

    Create Merge-Two-Arrays.js

---
+++ Arrays/Merge-Two-Arrays.js
@@ -0,0 +1,5 @@
+/*You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.*/
+
+function mergeArrays(arr1, arr2) {
+  return arr1+arr2
+}
