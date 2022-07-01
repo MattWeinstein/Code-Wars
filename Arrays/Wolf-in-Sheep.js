183fe9978ee58095902a563b6847f11f4932b2de
Matt Weinstein <mattw412@gmail.com>
2022-06-30 23:38:59 -0400

    Create Wolf-in-Sheep.js

---
+++ Arrays/Wolf-in-Sheep.js
@@ -0,0 +1,11 @@
+/*Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:*/
+
+function warnTheSheep(queue) {
+  if(queue[queue.length-1][0]==='w'){
+    return "Pls go away and stop eating my sheep"
+  } else{
+    return `Oi! Sheep number ${queue.length-queue.indexOf('wolf')-1}! You are about to be eaten by a wolf!`
+  }
+}
