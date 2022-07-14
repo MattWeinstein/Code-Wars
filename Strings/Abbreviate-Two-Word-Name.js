9963847712a31f38ca5a6fff05ef28e735e5afd3
Matt Weinstein <mattw412@gmail.com>
2022-07-13 22:30:14 -0400

    Create Abbreviate-Two-Word-Name.js

---
+++ Strings/Abbreviate-Two-Word-Name.js
@@ -0,0 +1,11 @@
+/*Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:*/
+
+function abbrevName(name){
+
+    return `${name.split(' ')[0][0].toUpperCase()}.${name.split(' ')[1][0].toUpperCase()}`;
+
+}
